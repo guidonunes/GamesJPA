b7ec51d4050af9ffdc210a330e97893ab34e17a8
Guilherme Nunes <guilherme.augd@gmail.com>
2026-04-24 16:30:13 -0300

    chore: setup environment variables

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,17 +1,24 @@
 package br.com.fiap;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Map<String, Object> props = new HashMap<>();
+
+        props.put("javax.persistence.jdbc.url", System.getenv("DB_URL"));
+        props.put("javax.persistence.jdbc.user", System.getenv("DB_USER"));
+        props.put("javax.persistence.jdbc.password", System.getenv("DB_PASSWORD"));
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("GamesJPA", props);
+
+        System.out.println("Connected successfully 🚀");
+
+        emf.close();
     }
 }
