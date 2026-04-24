ec50edb25c38e3115e0abfa08928706448feeddf
Guilherme Nunes <guilherme.augd@gmail.com>
2026-04-24 17:41:13 -0300

    chore: setup energy mananger

--- src/main/java/br/com/fiap/gamesjpa/Main.java
+++ src/main/java/br/com/fiap/gamesjpa/Main.java
@@ -1,7 +1,11 @@
 package br.com.fiap.gamesjpa;
 
+import br.com.fiap.gamesjpa.model.Game;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +21,26 @@ public class Main {
         EntityManagerFactory emf =
                 Persistence.createEntityManagerFactory("GamesJPA", props);
 
+        EntityManager em = emf.createEntityManager();
+
         System.out.println("Connected successfully 🚀");
 
+
+
+        Game game1 = new Game();
+        game1.setTitle("Spiderman");
+        game1.setReleaseDate(LocalDate.of(2023, 8, 24));
+        game1.setPrice(299.9);
+        game1.setDeveloper("Insomniac");
+        game1.setFinished(true);
+        game1.setCategory("Action");
+
+        em.getTransaction().begin();
+        em.persist(game1);
+        em.getTransaction().commit();
+        em.close();
         emf.close();
+        System.out.println("Game created successfully");
+        
     }
 }
